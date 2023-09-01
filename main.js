d5c020cbf42f779c4d4d489c32ee710d2bd424c7
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-09-01 17:15:14 -0300

    não terminado

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ const clientes = [
         mensagem: 'Posso realizar o pagamento por...',
         atendente: 'João Paulo',
         tempoEnvio: '30 minutos'
-    }
+    },
+    {
+        nome: 'Jerome Bueno',
+        notificacao: '2',
+        imagem: 'jerome.png',
+        mensagem: 'Posso realizar o pagamento por...',
+        atendente: 'João Paulo',
+        tempoEnvio: '30 minutos'
+    },
     {
         nome: 'Daniel Silva',
         notificacao: '1',
@@ -22,6 +30,13 @@ const clientes = [
 function criarChat (cliente){
     const container = document.getElementById('chat-container')
 
+    const chat = document.createElement('div')
+    chat.classList.add('chat')
+
+    const imagem = document.createElement('img')
+    imagem.src = `./img/${cliente.imagem}`  
+    imagem.alt = cliente.nome
+
     const conteudoMeio = document.createElement('div')
     conteudoMeio.classList.add('conteudo-meio')
 
@@ -33,23 +48,32 @@ function criarChat (cliente){
     
     const contato = document.createElement('h2')
     contato.classList.add('contato')
+    contato.textContent = cliente.nome
+
     const numeroMensagens = document.createElement('div')
     numeroMensagens.classList.add('numero-mensagens')
+    numeroMensagens.textContent = cliente.notificacao
 
     const tempoEnvio = document.createElement('p')
     tempoEnvio.classList.add('tempo-envio')
+    tempoEnvio.textContent = `Há ${tempoEnvio}`
 
     const mensagem = document.createElement('span')
     mensagem.classList.add('mensagem')
+    mensagem.textContent = cliente.mensagem
 
     const atendente = document.createElement('div')
     atendente.classList.add('atendente')
+    mensagem.textContent = cliente.atendente
 
+    tituloContato.replaceChildren(contato, numeroMensagens)
     cimaContato.replaceChildren(tituloContato, tempoEnvio)
 
     conteudoMeio.replaceChildren(cimaContato,mensagem, atendente)
 
-    container.appendChild(conteudoMeio)
+    chat.replaceChildren(conteudoMeio, imagem)
+
+    container.appendChild(chat)
 
 }
 
